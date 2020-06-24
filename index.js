396898d99fe46b12ec7dc4dc777305157d156f3f
sochiazuh <sochi.azuh@gmail.com>
2020-06-24 03:32:54 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//Example
+function orderMyLogic(val) {
+    if (val < 10){
+        return "Less than 10";
+    } else {
+        return "Not Less than 10";
+    }
+}
+console.log(orderMyLogic(17));
