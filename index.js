b751fcfe2a30d4d2811fd4dc08dbd64dfeec3f34
sochiazuh <sochi.azuh@gmail.com>
2020-06-25 03:58:58 +0100

    Variable Mutation/Variable Coersion

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-//Example
-function orderMyLogic(val) {
-    if (val < 10){
-        return "Less than 10";
-    } else {
-        return "Not Less than 10";
-    }
-}
-console.log(orderMyLogic(17));
+//Type coersion
+var firstName = 'John';
+var age = 28;
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + ' . Is he married?' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + ' . Is he married?' + isMarried);
+
+var lastName = prompt('What is his last name?');
+
+console.log(firstName + ' ' + lastName);
